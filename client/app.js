7b8c3b65f106a167caefd4784855b36806356916
Alessandro Arnodo <alessandro@arnodo.net>
2014-11-21 16:54:04 +0100

    some links

--- client/app.js
+++ client/app.js
@@ -15,6 +15,11 @@ router.init();
 //http://www.princeton.edu/~crmarsh/react-ssr/
 //http://scotch.io/tutorials/javascript/build-a-real-time-twitter-stream-with-node-and-react-js
 //http://jaketrent.com/post/send-props-to-children-react/
+//https://github.com/spikebrehm/isomorphic-tutorial
+//http://blog.risingstack.com/from-angularjs-to-react-the-isomorphic-way/
+//https://github.com/naman34/yarr
+//https://github.com/yahoo/flux-router-component
+//https://github.com/jaredly/github-issues-viewer
 /*
 React attempted to use reuse markup in a container but the checksum was invalid. This generally means that you are using server rendering and the markup generated on the server was not what the client was expecting. React injected new markup to compensate which works but you have lost many of the benefits of server rendering. Instead, figure out why the markup being generated is different on the client or server.
  */
