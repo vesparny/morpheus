ac279358ea50d617d9438a734568032fb687d63e
Alessandro Arnodo <alessandro@arnodo.net>
2014-11-24 20:18:14 +0100

    rename

--- client/components/Aside.jsx
+++ client/components/Aside.jsx
@@ -3,14 +3,13 @@
 'use strict';
 
 var React = require('react');
-var Link = require('react-router').Link;
 
 var Sidebar = React.createClass({
   render: function() {
     return (
       <div>
         <ul>
-          <li><Link to="/single">Dashboard</Link></li>
+          <li>aside</li>
         </ul>
       </div>
     );
