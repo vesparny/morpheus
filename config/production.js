a933c6b715d3e3479c30b30a704a3569889a5d25
Alessandro Arnodo <alessandro@arnodo.net>
2014-12-04 11:11:34 +0100

    production for demo site

--- config/production.js
+++ config/production.js
@@ -7,7 +7,7 @@ module.exports = {
     level: 'info',
     file: path.resolve(process.env.OPENSHIFT_DATA_DIR, 'log.log'),
   },
-  siteUrl: 'https:/alessandro.arnodo.net',
+  siteUrl: 'https://flashdemo-arnodo.rhcloud.com',
   port: process.env.OPENSHIFT_NODEJS_PORT,
   ip: process.env.OPENSHIFT_NODEJS_IP
 };
