39920e0d8a1409d0042f1159c56ad7f6602da243
Alessandro Arnodo <alessandro@arnodo.net>
2014-12-19 16:24:36 +0100

    up deps

--- server/routes/index.js
+++ server/routes/index.js
@@ -25,6 +25,7 @@ module.exports = function(server) {
   });
 
   server.get('/page/:page', function(req, res, next) {
+    console.log("type", typeof req.params.page);
     var page = req.params.page || '0';
     if (!validator.isInt(page)) {
       return next();
