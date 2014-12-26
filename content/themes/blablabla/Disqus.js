03aaad2f3a8ce5f5dbcda21b4afcd232c30f888a
Alessandro Arnodo <alessandro@arnodo.net>
2014-12-26 16:53:46 +0100

    fix disqus reset

--- content/themes/blablabla/Disqus.js
+++ content/themes/blablabla/Disqus.js
@@ -23,11 +23,22 @@ var Disqus = React.createClass({
     var disqus_title = this.props.title;//jshint ignore:line
     var disqus_url = this.props.url;//jshint ignore:line
 
-    var dsq = document.createElement('script');
-    dsq.type = 'text/javascript';
-    dsq.async = true;
-    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js'; //jshint ignore:line
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    if (window.DISQUS) {
+      window.DISQUS.reset({
+        reload: true,
+        config: function () {
+          this.page.identifier = disqus_identifier; // jshint ignore:line
+          this.page.url = disqus_url; // jshint ignore:line
+          this.page.title = disqus_title; // jshint ignore:line
+        }
+      });
+    }else{
+      var dsq = document.createElement('script');
+      dsq.type = 'text/javascript';
+      dsq.async = true;
+      dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js'; //jshint ignore:line
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    }
   },
   render: function() {
     return <div id="disqus_thread"></div>;
