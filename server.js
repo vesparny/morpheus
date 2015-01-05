580296e8b9ee92fc107e95ef90354a9328adfda2
Alessandro Arnodo <alessandro@arnodo.net>
2015-01-05 10:28:34 +0100

    ignoring map files

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ require('node-jsx').install();
 var morpheus = require('./morpheus');
 
 morpheus.run(function(info) {
-  morpheus.logger.info('Worker %s is running morpheus@%s in %s on port %d', process.pid, info.version, process.env.NODE_ENV || 'development', info.port);
+  morpheus.logger.info('Worker %s is running morpheus@%s in %s mode on port %d', process.pid, info.version, process.env.NODE_ENV || 'development', info.port);
 });
